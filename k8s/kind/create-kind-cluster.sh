c77ae8573d8d5872815638c9507c887754975a0e
SalindaGunarathna <salinda.eng@gmail.com>
2025-08-26 12:32:26 +0530

    test

--- k8s/kind/create-kind-cluster.sh
+++ k8s/kind/create-kind-cluster.sh
@@ -4,11 +4,9 @@ echo "=== Starting Kind Cluster ==="
 
 kind create cluster --name microservices-cloud-project --config kind-config.yaml
 
-
 echo "=== Loading Docker Images into Kind Cluster ==="
 
 chmod +x ./kind-load.sh
-
 ./kind-load.sh
 
 echo "=== Kind Cluster Started ==="
